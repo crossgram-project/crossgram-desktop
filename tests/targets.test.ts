9d5608ddef24ce9f37c92a419430a9247d32a343
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2026-08-15 05:30:35 +0800

    fix: disable ccache wrapper in dependencies

--- tests/targets.test.ts
+++ tests/targets.test.ts
@@ -86,6 +86,7 @@ describe("targets", () => {
     expect(release).toContain('text = text.replace("-j2 install", "-j4 install")');
     expect(release).toContain("make -j4 DESTDIR=/usr/src/breakpad-cache install");
     expect(release).toContain('f"target=/var/cache/ccache-{stage_name}"');
+    expect(release).toContain('text = text.replace("<<EOF\\n", "<<EOF\\nexport CCACHE_DISABLE=1\\n")');
     expect(release).toContain("stage_name = text[stage_name_start:stage_name_end]");
     expect(release).toContain("-j4 stage");
     expect(release).toContain("cp -a boost /usr/src/boost-cache/usr/local/include/");
